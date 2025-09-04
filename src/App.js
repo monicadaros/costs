2c04dfbcd7b024ac5828b3541f33e5d2ee840717
Monica <monicac2807@gmail.com>
2025-09-04 09:23:34 -0300

    props.children

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contato from './components/pages/Contato'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+import Container from './components/layout/Container'
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+<Router>
+  <ul>
+    <Link to='/'>Home</Link>
+    <Link to='/contato'>Contato</Link>
+    <Link to='/company'>Empresa</Link>
+    <Link to='/newproject'>Novo Projeto</Link>
+
+  </ul>
+  <Container customClass="min-height">
+    <Routes>
+      <Route exatc path='/' element={<Home/>}/>
+      <Route path='/contato' element={<Contato/>}></Route>
+      <Route path='/company' element={<Company/>}/>
+      <Route path='/newproject' element={<NewProject/>}/>
+    </Routes>
+  </Container>
+</Router>
   );
 }
 
